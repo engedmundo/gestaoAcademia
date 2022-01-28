d8ef30d97572ed81439619467ef4b77a46b2c38c
engedmundo <engedmundo@gmail.com>
2022-01-28 09:42:47 -0300

    gestao academia arquivos do heroku

--- gestaoAcademia/settings.py
+++ gestaoAcademia/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,7 +75,7 @@ WSGI_APPLICATION = 'gestaoAcademia.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -84,6 +85,11 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
     }
+}
+"""
+
+DATABASES = {
+    'default': dj_database_url.config()
 }
 
 
